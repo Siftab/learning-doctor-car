01463a372438f4e091a576ebfb8b677ec3d265e8
Siftab <siiffuuu@gmail.com>
2024-01-08 14:54:58 +0600

    table row implepented

--- src/Pages/Booking.jsx
+++ src/Pages/Booking.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../components/Providers/Authprovider';
+import TableRow from '../components/TableRow';
 
 const Booking = () => {
     const {user}=useContext(AuthContext);
@@ -15,6 +16,33 @@ const Booking = () => {
     return (
         <div>
             this is booking sectiosn{bookings.length}
+            <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox" />
+          </label>
+        </th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+     {
+        bookings.map((booking,idx)=><TableRow key={idx} props={booking}></TableRow>)
+     }
+     
+    </tbody>
+    
+    
+  </table>
+</div>
             
         </div>
     );
---
+++ src/components/TableRow.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const TableRow = ({props}) => {
+   console.log(props)
+const {UserName,date,price,img}=props;
+    return (
+        <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox" />
+          </label>
+        </th>
+        <td>
+          <div className="flex items-center gap-3">
+            <div className="avatar">
+              <div className="w-24 h-24">
+                <img src={img} alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+           
+          </div>
+        </td>
+        <td>
+         
+          <br/>
+          <span className="badge badge-ghost badge-sm">{UserName}</span>
+        </td>
+        <td>{price}</td>
+        <th>
+          <button className="btn btn-ghost btn-xs">details</button>
+        </th>
+      </tr>
+    );
+};
+
+export default TableRow;
